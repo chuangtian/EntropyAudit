119b09f98bd46aca3af904ff638caa3282d86a07
TianChuang <38176372+chuangtian@users.noreply.github.com>
2019-05-06 11:47:00 +0800

    chore: bump appsec

---
+++ src/entropyaudit/rationale.py
@@ -0,0 +1,6 @@
+"""Per-rule exploitability rationale for entropyaudit.
+
+The report must never degrade into a checkbox list, so every rule carries a
+written explanation of why the pattern is exploitable and what an attacker gains
+from it. These strings are the source of that text.
+"""
