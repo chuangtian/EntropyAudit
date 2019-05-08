1cf1bb5951cc055028f9624c441706192b715d07
TianChuang <38176372+chuangtian@users.noreply.github.com>
2019-05-08 11:27:00 +0800

    perf: improve python

--- src/entropyaudit/rationale.py
+++ src/entropyaudit/rationale.py
@@ -4,3 +4,6 @@ The report must never degrade into a checkbox list, so every rule carries a
 written explanation of why the pattern is exploitable and what an attacker gains
 from it. These strings are the source of that text.
 """
+
+from __future__ import annotations
+
