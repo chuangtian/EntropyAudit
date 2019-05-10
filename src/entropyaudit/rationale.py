7c855896827a09e2c42624427d853cf011cab5ca
TianChuang <38176372+chuangtian@users.noreply.github.com>
2019-05-10 14:23:00 +0800

    fix: correct python

--- src/entropyaudit/rationale.py
+++ src/entropyaudit/rationale.py
@@ -24,3 +24,6 @@ RATIONALE = {
         "recover the internal state and predict all future outputs. When the "
         "value guards access, such as a token, password, session id, salt, or "
         "nonce, use secrets or os.urandom instead."
+    ),
+    "EA003": (
+        "Seeding from the current time makes the sequence depend only on when "
