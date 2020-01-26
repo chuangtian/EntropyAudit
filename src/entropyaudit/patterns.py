d3290aa9597fa75f62b8ca1a256a000988e93a2d
TianChuang <38176372+chuangtian@users.noreply.github.com>
2020-01-26 09:11:00 +0800

    chore: nonce

---
+++ src/entropyaudit/patterns.py
@@ -0,0 +1,8 @@
+"""Rule definitions for entropyaudit.
+
+Each rule has a stable identifier, a severity, a CWE-style category, and a short
+title. The written exploitability rationale lives in rationale.py so the report
+always carries an explanation rather than a bare rule code.
+
+Severity ordering, from most to least serious: high, medium, low.
+"""
