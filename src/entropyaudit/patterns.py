b34f8992b24f3a20902de09d042abff762d58779
TianChuang <38176372+chuangtian@users.noreply.github.com>
2020-01-31 11:12:00 +0800

    chore: appsec

--- src/entropyaudit/patterns.py
+++ src/entropyaudit/patterns.py
@@ -6,3 +6,9 @@ always carries an explanation rather than a bare rule code.
 
 Severity ordering, from most to least serious: high, medium, low.
 """
+
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+
