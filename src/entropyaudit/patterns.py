74ba79e4158926a76f833287eebf833efe6fd84c
TianChuang <38176372+chuangtian@users.noreply.github.com>
2020-02-07 18:35:00 +0800

    docs: document security

--- src/entropyaudit/patterns.py
+++ src/entropyaudit/patterns.py
@@ -31,3 +31,9 @@ class Rule:
 RULES = {
     "EA001": Rule(
         rule_id="EA001",
+        title="Predictable seed passed to random generator",
+        severity="high",
+        category="Use of Insufficiently Random Values",
+        cwe="CWE-330",
+    ),
+    "EA002": Rule(
