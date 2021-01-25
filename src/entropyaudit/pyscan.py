abc7cd7dc54f4541bdacea23eb0020733c2eb8a1
TianChuang <38176372+chuangtian@users.noreply.github.com>
2021-01-25 13:20:00 +0800

    feat: add security

--- src/entropyaudit/pyscan.py
+++ src/entropyaudit/pyscan.py
@@ -53,3 +53,21 @@ class Finding:
     def sort_key(self) -> tuple:
         return (self.path, self.line, self.col, self.rule_id)
 
+
+@dataclass
+class _ImportInfo:
+    """Tracks how randomness and hashing modules were imported in a file."""
+
+    # Maps a local alias back to the canonical dotted module name.
+    module_aliases: dict = field(default_factory=dict)
+    # Maps a local name bound by "from x import y as z" to "x.y".
+    name_aliases: dict = field(default_factory=dict)
+    # All top-level modules imported, for file-level security context.
+    imported_modules: set = field(default_factory=set)
+
+
+def scan_source(source: str, path: str) -> list[Finding]:
+    """Parse source and return findings for the given display path.
+
+    A file that fails to parse yields no findings; the caller is told through
+    the returned parse error channel in scan_file.
