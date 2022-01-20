3d8fb723e2bae1ad33e6e0a86bdc08af2b9a0ab4
TianChuang <38176372+chuangtian@users.noreply.github.com>
2022-01-20 14:07:00 +0800

    fix: nonce

--- tests/test_entropyaudit.py
+++ tests/test_entropyaudit.py
@@ -31,3 +31,18 @@ class VulnerableSampleTests(unittest.TestCase):
     def setUp(self):
         findings, errors = cli.collect_findings(VULN)
         self.assertEqual(errors, [], "vulnerable sample should scan cleanly")
+        self.findings = findings
+
+    def test_vulnerable_sample_has_findings(self):
+        self.assertTrue(self.findings, "vulnerable sample must produce findings")
+
+    def test_expected_rule_coverage(self):
+        ids = set(f.rule_id for f in self.findings)
+        for expected in ("EA001", "EA002", "EA003", "EA004", "EA005", "EA006"):
+            self.assertIn(expected, ids, f"{expected} should fire on the vector")
+
+    def test_nonce_and_iv_both_flagged(self):
+        ea004 = [f for f in self.findings if f.rule_id == "EA004"]
+        self.assertEqual(len(ea004), 2, "both the IV and nonce constants fire EA004")
+
+    def test_total_count_is_seven(self):
